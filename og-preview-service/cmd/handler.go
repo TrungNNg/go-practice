1ceb604eb59b31f2fcf12dee34b9f50a630c6998
Trung Nguyen <ngtrung0916@gmail.com>
2025-02-17 14:02:37 -0800

    add fetchHTML

--- og-preview-service/cmd/handler.go
+++ og-preview-service/cmd/handler.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
+
+func fetchHTML(url string) (string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	html, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(html), nil
+}
 
 func OGHandler(w http.ResponseWriter, r *http.Request) {
 	// struct to hold json input
@@ -20,6 +36,23 @@ func OGHandler(w http.ResponseWriter, r *http.Request) {
 
 	// use the url and make the get request to get the html, parse the html to return go
 	// object
+	req, err := http.NewRequest("GET", input.URL, nil)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	//data2, _ := io.ReadAll(resp.Body)
+	//fmt.Println(string(data2))
+	defer resp.Body.Close()
+
+	// parse html
 
 	// Refactor: separate func
 	data := map[string]string{
--- og-preview-service/cmd/handler_test.go
+++ og-preview-service/cmd/handler_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestOGHander(t *testing.T) {
+func TestOGHandler(t *testing.T) {
 
 	t.Run("parse url from json", func(t *testing.T) {
 		inputJSON := `{"url":"https://www.imdb.com/title/tt27987046/"}`
@@ -34,6 +34,36 @@ func TestOGHander(t *testing.T) {
 		assertEqual(t, rs.Status, "200 OK")
 	})
 
+}
+
+// this handler will be passed into a httptest.Server, so that for different
+// path the server will return different html
+func mockHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/h1", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html><head><title>Mock Page</title></head><body><h1>Hello, World!</h1></body></html>`))
+	})
+	mux.HandleFunc("/h2", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html><head>Heelo</head></html>`))
+	})
+	return mux
+}
+
+func TestFetchHTML(t *testing.T) {
+	ms := mockServer()
+	got, _ := fetchHTML(ms.URL + "/h1")
+	want := `<html><head><title>Mock Page</title></head><body><h1>Hello, World!</h1></body></html>`
+
+	assertEqual(t, got, want)
+
+	got, _ = fetchHTML(ms.URL + "/h2")
+	want = `<html><head>Heelo</head></html>`
+
+	assertEqual(t, got, want)
+}
+
+func mockServer() *httptest.Server {
+	return httptest.NewServer(mockHandler())
 }
 
 func newPostRequest(json string) *http.Request {
